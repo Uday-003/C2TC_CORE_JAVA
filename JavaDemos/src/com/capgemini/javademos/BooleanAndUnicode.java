bf40d9c17eb44502da30990e2e31981fef9f3c3d
Uday-003 <udaytiwaskar03@gmail.com>
2022-10-20 09:03:07 +0530

    First Commit

---
+++ JavaDemos/src/com/capgemini/javademos/BooleanAndUnicode.java
@@ -0,0 +1,16 @@
+//This program demonstrate Boolean and unicode
+package com.capgemini.javademos;
+
+public class BooleanAndUnicode {
+
+	public static void main(String[] args) {
+		boolean boolVal = false;
+		System.out.println(boolVal);
+
+		char unicodeVal = '\u00A7';//This is the value for the special symbol
+		System.out.println(unicodeVal);
+
+
+	}
+
+}
